bb863f468d5150ffb1863b64e1a66f4e8a233c08
JF Denise <jdenise@redhat.com>
2019-10-03 21:57:58 +0200

    ejb tx recovery initial commit

---
+++ jboss/container/wildfly/galleon/fp-content/ejb-tx-recovery/added/src/main/resources/packages/wildfly.s2i.ejb.tx.recovery/content/bin/launch/statefulset.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+source $JBOSS_HOME/bin/launch/logging.sh
+
+function prepareEnv() {
+  unset STATEFULSET_HEADLESS_SERVICE_NAME
+}
+
+function configure() {
+  if [ -n "$STATEFULSET_HEADLESS_SERVICE_NAME" ]; then
+    configure_socket_binding
+  fi
+}
+
+function configureEnv() {
+  configure
+}
+
+function configure_socket_binding)( {
+   cat << EOF >> ${CLI_SCRIPT_FILE}
+    if (outcome != success) of /socket-binding-group=standard-sockets/socket-binding=http:read-resource
+      echo You have set STATEFULSET_HEADLESS_SERVICE_NAME although http socket-binding is not configured. >> \${error_file}
+      exit
+    else
+        /socket-binding-group=standard-sockets/socket-binding=http:list-add(name=client-mappings,value={destination-address=$STATEFULSET_HEADLESS_SERVICE_NAME})
+    end-if
+    if (outcome != success) of /socket-binding-group=standard-sockets/socket-binding=https:read-resource
+      echo You have set STATEFULSET_HEADLESS_SERVICE_NAME although https socket-binding is not configured. >> \${error_file}
+      exit
+    else
+        /socket-binding-group=standard-sockets/socket-binding=https:list-add(name=client-mappings,value={destination-address=$STATEFULSET_HEADLESS_SERVICE_NAME})
+    end-if
+EOF
+}
