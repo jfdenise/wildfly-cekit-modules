9cac5a29469521f5ec683cdf9fac7f61b3cb6290
JF Denise <jdenise@redhat.com>
2019-12-09 14:20:43 +0100

    Fix for issue 143, generated keycloak feature group

--- jboss/container/wildfly/galleon/fp-content/keycloak/configure.sh
+++ jboss/container/wildfly/galleon/fp-content/keycloak/configure.sh
@@ -5,4 +5,19 @@ set -e
 SCRIPT_DIR=$(dirname $0)
 ADDED_DIR=${SCRIPT_DIR}/added
 
+# Generate the set of keycloak packages.
+pushd "$JBOSS_HOME/modules/system/add-ons/keycloak/" &> /dev/null
+target_dir=/tmp/keycloak_modules.txt
+find -name module.xml -printf '%P\n' > "$target_dir"
+while read line; do
+  parentdir="$(dirname $line)"
+  parentdir="$(dirname $parentdir)"
+  modulename=${parentdir//\//.}
+  pkgs="$pkgs<package name=\"$modulename\"/>\n"
+done < "$target_dir"
+rm "$target_dir"
+popd
+
+sed -i "s|<!-- ##KEYCLOAK_PACKAGES## -->|$pkgs|" "${ADDED_DIR}/src/main/resources/feature_groups/keycloak.xml"
+
 cp -r ${ADDED_DIR}/* ${GALLEON_FP_PATH}
